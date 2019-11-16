8bfa696695aada92008ee5ed57618e964aca78e0
Rapsssito <rodrigo712000@gmail.com>
2019-11-16 20:47:22 +0100

    Little TS support

--- index.d.ts
+++ index.d.ts
@@ -4,11 +4,11 @@ import Socket from './TcpSocket';
 import Server from './TcpServer';
 
 export default class TCPSockets {
-    static createServer(connectionListener) {
+    static createServer(connectionListener?: void) {
         return new Server(connectionListener);
     }
 
-    static createConnection(options, callback) {
+    static createConnection(options: any, callback?: void) {
         const tcpSocket = new Socket();
         return tcpSocket.connect(options, callback);
     }
