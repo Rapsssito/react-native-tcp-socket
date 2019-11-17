10a9d394679ca2409d32acacc464e1e700679cbc
Rapsssito <rodrigo712000@gmail.com>
2019-11-17 14:16:53 +0100

    Initial test setup

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: 'react-native',
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/examples/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
