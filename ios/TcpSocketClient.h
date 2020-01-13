6041ee1b8223f84ed619e86e5139880793b89e59
Rapsssito <contact@rodrigomartin.dev>
2020-01-13 19:19:53 +0100

    Changed to local CocoaAsyncSockets to prevent duplicate symbols

--- ios/TcpSocketClient.h
+++ ios/TcpSocketClient.h
@@ -4,7 +4,7 @@
  */
 
 #import <React/RCTBridgeModule.h>
-#import "GCDAsyncSocket.h"
+#import "CocoaAsyncSocket/GCDAsyncSocket.h"
 
 extern NSString *const RCTTCPErrorDomain;
 
--- react-native-tcp-socket.podspec
+++ react-native-tcp-socket.podspec
@@ -20,7 +20,6 @@ Pod::Spec.new do |s|
   s.requires_arc = true
 
   s.dependency "React"
-  s.dependency "CocoaAsyncSocket"
   
   # s.dependency "..."
 end
