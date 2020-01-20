25f5074e5f7f54179bbe66343aee3801183642ba
Rapsssito <contact@rodrigomartin.dev>
2020-01-20 20:15:19 +0100

    Tests mocks (fixes: #9)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     preset: 'react-native',
+    collectCoverage: true,
     testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/examples/'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    collectCoverageFrom: ['./src/**/*.{js, ts}'],
+    globals: {
+        __DEV__: true,
+    },
+    timers: 'fake',
 };
