ecf2468e88845f077228f1d7f310a62c40ee237e
Rodrigo Martin <contact@rodrigomartin.dev>
2020-02-19 19:18:54 +0100

    chore: Add index.js tests (#23)

---
+++ jest.setup.js
@@ -0,0 +1,21 @@
+jest.mock('react-native', () => {
+    // Mock NativeEventEmitter
+    class NativeEventEmitter {
+        constructor() {
+            this.addListener = jest.fn(() => ({ remove: () => {} }));
+        }
+    }
+
+    return {
+        NativeModules: {
+            TcpSockets: {
+                connect: jest.fn(),
+                end: jest.fn(),
+                destroy: jest.fn(),
+                write: jest.fn(),
+                listen: jest.fn(),
+            },
+        },
+        NativeEventEmitter: NativeEventEmitter,
+    };
+});
