5498814022272385ee8e2492f998c58d12773a43
Rodrigo Martin <contact@rodrigomartin.dev>
2021-10-26 21:42:38 +0200

    feat: Add isIP & isIPv4 & isIPv6 methods (#133)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -19,3 +19,19 @@ test('create-server', () => {
     const server = net.createServer(() => {});
     expect(server).toBeInstanceOf(net.Server);
 });
+
+test('isIP', () => {
+    expect(net.isIP('127.9.8.9')).toBe(4);
+    expect(net.isIP('127.9.8..')).toBe(0);
+    expect(net.isIP('2001:0db8:85a3:0000:0000:8a2e:0370:7334')).toBe(6);
+});
+
+test('isIPv4', () => {
+    expect(net.isIPv4('127.9.8.9')).toBeTruthy();
+    expect(net.isIPv4('127.9.8.9999')).toBeFalsy();
+});
+
+test('isIPv6', () => {
+    expect(net.isIPv6('2001:0db8:85a3:0000:0000:8a2e:0370:7334')).toBeTruthy();
+    expect(net.isIPv6('2001:0db8:85a3:hhhh:0000:8a2e:0370:7334')).toBeFalsy();
+});
